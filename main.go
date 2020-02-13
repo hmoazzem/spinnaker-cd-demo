8060738f306938e5eeefc0b857900f76121ccdc5
Moazzem Hossen <mhossen.ndc@gmail.com.com>
2020-02-13 12:15:06 +0100

    first

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Spinnaker CD demo")
+        fmt.Fprintf(w, `<h1 style="color:blue;">V1</h1>`)
 	})
 	
     http.ListenAndServe(":80", nil)
